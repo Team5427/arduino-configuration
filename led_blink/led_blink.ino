3afabe5940b947b8163bc13feaa6457c1018cadc
Charlemagne Wong <frian13@hotmail.com>
2017-02-02 20:46:29 -0600

    Change blink rate

--- led_blink/led_blink.ino
+++ led_blink/led_blink.ino
@@ -1,4 +1,4 @@
-#include <libusb.h>
+//#include <libusb.h>
 
 void setup() {
   // put your setup code here, to run once:
@@ -8,10 +8,23 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
+
+  for (int i = 0; i < 1; ++i) {
   digitalWrite(LED_BUILTIN, HIGH);
-  delay(500);
+  delay(1000);
   digitalWrite(LED_BUILTIN, LOW);
   delay(500);
+  }
+
+  for (int i = 0; i < 3; ++i) {
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(100);
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(100);
+  }
+
+  delay(3000);
+  
   Serial.print("looping");
   
 }
